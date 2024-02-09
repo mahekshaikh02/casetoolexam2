e7c920035b1c936186be25a8196096bfa5f77fba
Mahek <mahekshk02@gmail.com>
2024-02-09 12:30:45 +0530

    commit

---
+++ casetools2/src/casetools2/caterpillar_218.java
@@ -0,0 +1,13 @@
+package casetools2;
+
+public class caterpillar_218 {
+	
+	int moves,cabbages;
+	
+	caterpillar_218(int moves,int cabbages)
+	{
+		this.moves=moves;
+		this.cabbages=cabbages;
+	}
+
+}
